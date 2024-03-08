e5c4353a7acc5c24917b8e36637e1fc99e850a3d
phillomella <121259223+phillomella@users.noreply.github.com>
2024-03-08 22:50:20 +0300

    upd HomeScreen

--- app/src/main/java/com/topic3/android/reddit/screens/HomeScreen1.kt
+++ app/src/main/java/com/topic3/android/reddit/screens/HomeScreen1.kt
@@ -1,51 +1,47 @@
 package com.topic3.android.reddit.screens
 
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.topic3.android.reddit.components.ImagePost
 import com.topic3.android.reddit.components.TextPost
 import com.topic3.android.reddit.domain.model.PostModel
 import com.topic3.android.reddit.domain.model.PostType
 import com.topic3.android.reddit.viewmodel.MainViewModel
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Card
-import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.Icon
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import com.topic3.android.reddit.R
-import com.topic3.android.reddit.components.JoinedToast
 import com.topic3.android.reddit.views.TrendingTopicView
 import java.util.Timer
 import kotlin.concurrent.schedule
-
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
 
 private val trendingItems = listOf( TrendingTopicModel(
     "Compose Tutorial",
@@ -72,6 +68,7 @@ private val trendingItems = listOf( TrendingTopicModel(
         R.drawable.deeplinking
     )
 )
+
 @Composable
 fun HomeScreen(viewModel: MainViewModel) {
     val posts: List<PostModel>
@@ -119,7 +116,6 @@ fun HomeScreen(viewModel: MainViewModel) {
         )
     }
 }
-
 private fun mapHomeScreenItems(
     posts: List<PostModel>
 ): List<HomeScreenItem> {
@@ -137,47 +133,35 @@ private fun mapHomeScreenItems(
     return homeScreenItems
 }
 
-
 private data class HomeScreenItem(
-    val type: HomeScreenItem,
-    val post :PostModel? =null
+    val type: HomeScreenItemType,
+    val post: PostModel? = null
 )
 
-private enum class HomeScreenItemType{
+private enum class HomeScreenItemType {
     TRENDING,
     POST
 }
+
 private data class TrendingTopicModel(
     val text: String,
-    @DrawableRes val imageRes: Int= 0
+    @DrawableRes val imageRes: Int = 0
 )
+
 @Composable
-private fun TrendingTopic(trendingTopic: TrendingTopicModel)
-{
+private fun TrendingTopic(trendingTopic: TrendingTopicModel) {
     AndroidView({context ->
         TrendingTopicView(context).apply {
-            text =trendingTopic.text
-            image =trendingTopic.imageRes
+            text = trendingTopic.text
+            image = trendingTopic.imageRes
         }
     })
 }
-
 
 @Preview
 @Composable
 private fun TrendingTopicsPreview() {
     TrendingTopics(trendingTopics = trendingItems)
-}
-
-@Preview (showBackground = true)
-@Composable
-private fun TrendingTopicPreview(){
-    TrendingTopic(
-    trendingTopic = TrendingTopicModel(
-        "Compose Animation",
-        R.drawable.jetpack_compose_animations
-    )
-    )
 }
 
 @Composable
@@ -223,11 +207,12 @@ private fun TrendingTopics(
                             if (index != trendingTopics.lastIndex) {
                                 Spacer(modifier = Modifier.width(8.dp))
                             }
+
                         }
                     )
                 }
             )
         }
     }
-}
 
+}
